0cd919984f9ec590f9a61ffd5d4f5cca3a9a57c8
Rajesh <mail2rajeshgundu@gmail.com>
2025-03-14 16:42:12 +0530

    Time: 41 ms (90.66%) | Memory: 28.9 MB (85.21%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,6 +5,7 @@ public:
         int n = nums.size();
         vector<vector<int>> ans;
         for(int i=0;i<n;i++) {
+            if(nums[i] > 0) break;
             if(i != 0 && nums[i] == nums[i-1]) continue;
             int l = i+1;
             int r = n-1;
