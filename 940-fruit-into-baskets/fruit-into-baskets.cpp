9b013a3edb0389ad0f16c197aeecc8b6dc381737
Rajesh <mail2rajeshgundu@gmail.com>
2025-08-04 20:13:00 +0530

    Time: 874 ms (5.25%) | Memory: 69.9 MB (98.21%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int idx1 = 0;
+        int idx2 = -1;
+        int i = 0;
+        int j = 1;
+        int ans = 1;
+        while(j < n) {
+            if(fruits[j] == fruits[idx1]) {
+                idx1 = j;
+            }
+            else if(idx2 == -1 || fruits[j] == fruits[idx2]) {
+                idx2 = j;
+            }
+            else {
+                cout << i << " " << j  << " " << idx1  << " " << idx2 << endl;
+                i = min(idx1,idx2)+1;
+                idx1 = (i == idx1+1 ? idx2 : idx1);
+                idx2 = j;
+            }
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+};
