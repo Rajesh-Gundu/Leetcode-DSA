504e59fbbb59769092d6c4fd765110ce79965477
Rajesh <mail2rajeshgundu@gmail.com>
2025-08-08 11:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (14.77%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -3,7 +3,7 @@ public:
     int kthGrammar(int n, int k) {
         if(k == 1)
             return 0;
-        int mid = (1<<(n-1));
+        int mid = (1<<(n-2));
         if(k <= mid)
             return kthGrammar(n-1,k);
         return !(kthGrammar(n-1,k-mid));
