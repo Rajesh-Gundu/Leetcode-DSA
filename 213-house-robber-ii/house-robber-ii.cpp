490e8ba26222ff1ce406eac7bb33251ff49da0df
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-19 17:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (20.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -7,16 +7,28 @@ class Solution {
         int skip = memoi(n-1,nums,dp);
         return dp[n] = max(include,skip);
     }
+
+    int tabulation(int n,vector<int>& nums) {
+        if(n == 1)
+            return nums[0];
+        vector<int> dp(n,-1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++) {
+            int include = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = max(include,skip);
+        }
+        return dp[n-1];
+    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1)
             return nums[0];
-        vector<int> dp(n,-1);
-        int s1 = memoi(n-2,nums,dp);
+        int s1 = tabulation(n-1,nums);
         nums[0] = 0;
-        fill(dp.begin(),dp.end(),-1);
-        int s2 = memoi(n-1,nums,dp);
+        int s2 = tabulation(n,nums);
         return max(s1,s2);
     }
 };
