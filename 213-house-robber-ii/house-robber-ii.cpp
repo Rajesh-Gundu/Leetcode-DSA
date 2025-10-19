52f98ca5072a603e4de25ad8c025de6c5ac87f83
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-19 17:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.18%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -11,15 +11,16 @@ class Solution {
     int tabulation(int n,vector<int>& nums) {
         if(n == 1)
             return nums[0];
-        vector<int> dp(n,-1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
         for(int i=2;i<n;i++) {
-            int include = nums[i] + dp[i-2];
-            int skip = dp[i-1];
-            dp[i] = max(include,skip);
+            int include = nums[i] + prev2;
+            int skip = prev1;
+            int cur = max(include,skip);
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[n-1];
+        return prev1;
     }
 public:
     int rob(vector<int>& nums) {
