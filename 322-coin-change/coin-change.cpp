11f13f62a297e6da7ad9bc340a5412e85e9b6960
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-22 16:48:14 +0530

    Time: 43 ms (43.17%) | Memory: 21.9 MB (52.90%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        queue<pair<int,int>> q;
+        q.push({0,amount});
+        vector<bool> vis(10001,false); 
+        vis[amount] = true;
+        while(!q.empty()) {
+            int steps = q.front().first;
+            int value = q.front().second;
+            q.pop();
+            if(value == 0)
+                return steps;
+            for(int& c : coins) {
+                if(c <= value && !vis[value-c]) {
+                    q.push({steps+1,value-c});
+                    vis[value-c] = true;
+                }
+            }
+        }
+        return -1;
+    }
+};
