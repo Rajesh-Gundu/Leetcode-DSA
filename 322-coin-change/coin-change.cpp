738be27b75e5731c2bdd737f104af7cc9abb5eee
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-22 17:45:14 +0530

    Time: 24 ms (65.14%) | Memory: 18.9 MB (60.68%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,26 @@
 class Solution {
+    int memoi(vector<int>& coins, int amount,vector<int>& dp) {
+        if(amount < 0)  return 1e7;
+        if(amount == 0) return dp[amount] = 0;
+        if(dp[amount] != -1)    return dp[amount];
+
+        int res = INT_MAX;
+        for(auto& c : coins) {
+            int take = memoi(coins,amount-c,dp);
+            if(take >= 0)
+                res = min(res,take+1);
+        }
+
+        if(res >= 1e7)
+            return dp[amount] = -2;
+        return dp[amount] = res;
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        queue<pair<int,int>> q;
-        q.push({0,amount});
-        vector<bool> vis(amount+1,false); 
-        vis[amount] = true;
-        while(!q.empty()) {
-            int steps = q.front().first;
-            int value = q.front().second;
-            q.pop();
-            if(value == 0)
-                return steps;
-            for(int& c : coins) {
-                if(c <= value && !vis[value-c]) {
-                    q.push({steps+1,value-c});
-                    vis[value-c] = true;
-                }
-            }
-        }
-        return -1;
+        vector<int> dp(amount+1,-1);
+        memoi(coins,amount,dp);
+        if(dp[amount] == -2)
+            return -1;
+        return dp[amount];
     }
 };
