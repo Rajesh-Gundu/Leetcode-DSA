d14330aa24c18b3ba53696767866ff784eaa6c80
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-23 13:23:56 +0530

    Time: 31 ms (37.17%) | Memory: 36.7 MB (32.11%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int solve(int idx,int amount,vector<int>& coins,vector<vector<int>>& dp) {
+        if(idx == 0)    return (amount%coins[0] == 0);
+        if(dp[idx][amount] != -1)   return dp[idx][amount];
+
+        int take = 0;
+        if(amount >= coins[idx])
+            take = solve(idx,amount-coins[idx],coins,dp);
+        int skip = solve(idx-1,amount,coins,dp);
+        return dp[idx][amount] = take + skip;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+       int n = coins.size();
+        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
+        return solve(n-1,amount,coins,dp);
+    }
+};  
