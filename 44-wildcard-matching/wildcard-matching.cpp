415eb40e3eac495c579bbc648a5bbc77971c0504
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-23 23:13:16 +0530

    Time: 59 ms (10.68%) | Memory: 52.3 MB (5.10%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -9,23 +9,22 @@ public:
         int n1 = s1.length();
         int n2 = s2.length();
 
-        vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
-        dp[0][0] = 1;
+        vector<int> prev(n2+1,0);
+        prev[0] = 1;
 
         for(int i=1;i<=n1;i++) {
+            vector<int> cur(n2+1,0);
             if(allStars(i-1,s1))
-                dp[i][0] = 1;
-        }
-
-        for(int i=1;i<=n1;i++) {
+                cur[0] = 1;
             for(int j=1;j<=n2;j++) {
                 if(s1[i-1] == s2[j-1] || s1[i-1] == '?')
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 else if(s1[i-1] == '*')
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                    cur[j] = cur[j-1] || prev[j];
             }
+            prev = cur;
         }
 
-        return dp[n1][n2];
+        return prev[n2];
     }
 };
