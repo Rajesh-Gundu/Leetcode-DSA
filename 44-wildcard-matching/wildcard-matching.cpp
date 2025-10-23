442f13dd2ac8d79fbfdce91ea5210f7fbc283213
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-23 23:11:30 +0530

    Time: 57 ms (11.10%) | Memory: 53.8 MB (5.02%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,26 +4,28 @@ class Solution {
         return i < 0;
     }
 
-    bool solve(int i,int j,string& s1, string& s2,vector<vector<int>>& dp) {
-        if(i < 0 && j < 0)  return true;
-        if(i < 0)   return false;
-        if(j < 0)   return allStars(i,s1);
-        if(dp[i][j] != -1)  return dp[i][j];
-
-        if(s1[i] == s2[j] || s1[i] == '?')
-            return dp[i][j] = solve(i-1,j-1,s1,s2,dp);
-        else if(s1[i] == '*')
-            return dp[i][j] = solve(i,j-1,s1,s2,dp) || solve(i-1,j,s1,s2,dp);
-        return false;
-    }
-
 public:
     bool isMatch(string s2, string s1) {
         int n1 = s1.length();
         int n2 = s2.length();
 
-        vector<vector<int>> dp(n1,vector<int> (n2,-1));
+        vector<vector<int>> dp(n1+1,vector<int> (n2+1,0));
+        dp[0][0] = 1;
 
-        return solve(n1-1,n2-1,s1,s2,dp);
+        for(int i=1;i<=n1;i++) {
+            if(allStars(i-1,s1))
+                dp[i][0] = 1;
+        }
+
+        for(int i=1;i<=n1;i++) {
+            for(int j=1;j<=n2;j++) {
+                if(s1[i-1] == s2[j-1] || s1[i-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(s1[i-1] == '*')
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+            }
+        }
+
+        return dp[n1][n2];
     }
 };
