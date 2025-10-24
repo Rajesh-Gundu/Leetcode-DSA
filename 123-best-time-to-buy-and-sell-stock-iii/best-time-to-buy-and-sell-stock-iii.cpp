cf706c21eba822836548d59155d3c1c48fe930d5
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-24 13:56:27 +0530

    Time: 176 ms (54.12%) | Memory: 138.7 MB (55.23%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,27 @@
 class Solution {
+    int solve(int i,int t,vector<int>& prices,vector<vector<int>>& dp) {
+        if(i == prices.size() || t == 4)    return 0;
+        if(dp[i][t] != -1)  return dp[i][t];
+
+        if(t%2 == 0) {
+            int include = -prices[i] + solve(i+1,t+1,prices,dp);
+            int skip = solve(i+1,t,prices,dp);
+            return dp[i][t] = max(include,skip);
+        }
+        else {
+            int include = prices[i] + solve(i+1,t+1,prices,dp);
+            int skip = solve(i+1,t,prices,dp);
+            return dp[i][t] = max(include,skip);
+        }
+    }
+
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+        vector<vector<int>> dp(n,vector<int> (4,-1));
 
-        for(int i=n-1;i>=0;i--) {
-            for(int j=0;j<2;j++) {
-                for(int k=1;k>=0;k--) {
-                    if(j == 0) {
-                        int include = prices[i] + dp[i+1][1][k+1];
-                        int skip = dp[i+1][0][k];
-                        dp[i][j][k] = max(include,skip);
-                    }
-                    else {
-                        int include = -prices[i] + dp[i+1][0][k];
-                        int skip = dp[i+1][1][k];
-                        dp[i][j][k] = max(include,skip);
-                    }
-                }
-            }
-        }
-        return dp[0][1][0];
+        return solve(0,0,prices,dp);
+
+        
     }
 };
