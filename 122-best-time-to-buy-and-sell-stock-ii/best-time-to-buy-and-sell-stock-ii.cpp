1c7437cdc61c00f8c1e8073570651df1e7496b57
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-24 13:00:08 +0530

    Time: 3 ms (36.23%) | Memory: 19.8 MB (20.81%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
-    int solve(int idx,int buy,vector<int>& prices,vector<vector<int>>& dp) {
-        if(idx == prices.size())    return 0;
-        if(dp[idx][buy] != -1)  return dp[idx][buy];
-
-        if(buy) {
-            int include = -prices[idx] + solve(idx+1,0,prices,dp);
-            int skip = solve(idx+1,1,prices,dp);
-            return dp[idx][buy] = max(include,skip);
-        }
-        else {
-            int include = prices[idx] + solve(idx+1,1,prices,dp);
-            int skip = solve(idx+1,0,prices,dp);
-            return dp[idx][buy] = max(include,skip);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        for(int i=n-1;i>=0;i--) {
+            for(int j=0;j<2;j++) {
+                if(j == 0) {
+                    int include = prices[i] + dp[i+1][1];
+                    int skip = dp[i+1][0];
+                    dp[i][j] = max(include,skip);
+                }
+                else {
+                    int include = -prices[i] + dp[i+1][0];
+                    int skip = dp[i+1][1];
+                    dp[i][j] = max(include,skip);
+                }
+            }
+        }
+
+        return dp[0][1];
     }
 };
