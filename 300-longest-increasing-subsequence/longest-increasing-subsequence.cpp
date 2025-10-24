d4d7427452ba20df11a9f3145c35e86a86e15de6
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-24 16:24:25 +0530

    Time: 3 ms (80.84%) | Memory: 14.1 MB (87.51%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,30 @@
 class Solution {
+    int getIdx(vector<int>& lis,int num) {
+        int lo = 0;
+        int hi = lis.size()-1;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(lis[mid] < num) {
+                lo = mid + 1;
+            }
+            else
+                hi = mid - 1;
+        } 
+        return lo;
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1);
-        int maxi = 1;
-        for(int i=1;i<n;i++) {
-            for(int j=0;j<i;j++) {
-                if(nums[i] > nums[j] && dp[j]+1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    maxi = max(maxi,dp[i]);
-                }
+        vector<int> lis;
+        for(int i=0;i<n;i++) {
+            if(i == 0 || lis.back() < nums[i])
+                lis.push_back(nums[i]);
+            else {
+                int idx = getIdx(lis,nums[i]);
+                lis[idx] = nums[i];
             }
+
         }
-        return maxi;
+        return lis.size();
     }
 };
