88440a1ef2e55d8fa2cdceff061a1d704fdae6e4
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-24 13:45:52 +0530

    Time: 388 ms (49.35%) | Memory: 211.3 MB (50.77%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,25 @@
 class Solution {
-    int solve(int i,int buy,int k,vector<int>& prices,vector<vector<vector<int>>>& dp) {
-        if(i == prices.size() || k == 2) return 0;
-        if(dp[i][buy][k] != -1) return dp[i][buy][k];
-
-        if(buy) {
-            int include = -prices[i] + solve(i+1,0,k,prices,dp);
-            int skip = solve(i+1,1,k,prices,dp);
-            return dp[i][buy][k] = max(include,skip);
-        }
-        else {
-            int include = prices[i] + solve(i+1,1,k+1,prices,dp);
-            int skip = solve(i+1,0,k,prices,dp);
-            return dp[i][buy][k] = max(include,skip);
-        }
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (2,-1)));
-        return solve(0,1,0,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+
+        for(int i=n-1;i>=0;i--) {
+            for(int j=0;j<2;j++) {
+                for(int k=1;k>=0;k--) {
+                    if(j == 0) {
+                        int include = prices[i] + dp[i+1][1][k+1];
+                        int skip = dp[i+1][0][k];
+                        dp[i][j][k] = max(include,skip);
+                    }
+                    else {
+                        int include = -prices[i] + dp[i+1][0][k];
+                        int skip = dp[i+1][1][k];
+                        dp[i][j][k] = max(include,skip);
+                    }
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };
