c7b06353300a5600cf04a0e1b7d8f3883dfec4fc
Rajesh <mail2rajeshgundu@gmail.com>
2025-10-24 13:08:46 +0530

    Time: 3 ms (36.23%) | Memory: 18.4 MB (34.00%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,18 +6,15 @@ public:
         
         for(int i=n-1;i>=0;i--) {
             vector<int> cur(2,0);
-            for(int j=0;j<2;j++) {
-                if(j == 0) {
-                    int include = prices[i] + next[1];
-                    int skip = next[0];
-                    cur[j] = max(include,skip);
-                }
-                else {
-                    int include = -prices[i] + next[0];
-                    int skip = next[1];
-                    cur[j] = max(include,skip);
-                }
-            }
+            
+            int include = prices[i] + next[1];
+            int skip = next[0];
+            cur[0] = max(include,skip);
+
+            include = -prices[i] + next[0];
+            skip = next[1];
+            cur[1] = max(include,skip);
+            
             next = cur;
         }
 
